3b4eee4a2e0be1f081f4dabb406368c190858b8b
ImraanKhann <imraannkhan0@gmail.com>
2025-02-17 20:35:52 +0530

    Added Orderblock strategy from StockWiz

---
+++ OrderBlock.py
@@ -0,0 +1,172 @@
+from pickle import NONE
+import time
+import requests
+import asyncio
+import pytz
+import json
+import math
+from datetime import datetime
+from pytz import timezone
+import numpy as np
+import pandas as pd
+import pytz
+from telegram import Bot
+import logging
+import yfinance as yf
+
+now = datetime.now()
+format = "%d-%m-%Y %H:%M:%S %Z%z"
+now_utc = datetime.now(timezone('UTC'))
+now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
+ocTime = now_asia.strftime(format)
+fName = now_asia.strftime(format)
+tdate = fName.split(" IST")
+nowTime = tdate[0]
+print(nowTime)
+curWeekday = datetime.today().weekday()
+dtTime = fName.split(" IST")
+dt = dtTime[0].split(" ")
+dtWithOutTime = dt[0].split(" ")
+dateWithOutTime = dtWithOutTime[0]
+isHolidayNxtDay = ""
+reqTime = ocTime[11:16]
+reqMin = ocTime[14:16]
+intTime = int(reqTime[0:2])
+intMin = int(reqMin)
+print("Int min : ", intMin)
+counter = 0
+logFileName = dateWithOutTime+"-"+"MagicLevel.log"
+
+TELEGRAM_BOT_TOKEN = "5817461626:AAHp1IIIMkQGWFTqIuu84lYOoxlO8KS7CZo"
+TELEGRAM_CHAT_ID = "@swingTradeScreenedStocks"
+
+#Fetch Updated Index values from csv file
+nsedf_minus_supply_level = pd.read_csv('./levels_for_orderblock.csv',usecols=['minusSupplyLevel'],nrows=1)
+nsedf_plus_supply_level = pd.read_csv('./levels_for_orderblock.csv',usecols=['plusSupplyLevel'],nrows=1)
+nsedf_minus_demand_level = pd.read_csv('./levels_for_orderblock.csv',usecols=['minusDemandLevel'],nrows=1)
+nsedf_plus_demand_level = pd.read_csv('./levels_for_orderblock.csv',usecols=['plusDemandLevel'],nrows=1)
+nse_ce_risky_levels = int(nsedf_minus_supply_level['minusSupplyLevel'].loc[nsedf_minus_supply_level.index[0]])
+nse_ce_safe_levels = int(nsedf_plus_supply_level['plusSupplyLevel'].loc[nsedf_plus_supply_level.index[0]])
+nse_pe_safe_levels = int(nsedf_minus_demand_level['minusDemandLevel'].loc[nsedf_minus_demand_level.index[0]])
+nse_pe_risky_levels = int(nsedf_plus_demand_level['plusDemandLevel'].loc[nsedf_plus_demand_level.index[0]])
+
+# Function to fetch Nifty data
+def fetch_nifty_data():
+    # Fetch Nifty data (use "period=3d" for buffer)
+    nifty_data = yf.download('^NSEI', period='3d', interval='1d')
+    return nifty_data
+
+# Function to send Telegram notification
+async def send_telegram_notification(message):
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+
+ #Notify Index values To Telegram Channel after 9:15AM
+if intTime==9 and intMin in range(15,45):
+    message = (f'===============================\n'
+              f'Order Block detected at {nowTime} IST.\n'
+              f'===============================\n'
+              f'AI BOT STARTED SUCCESSFULLY..!\n'
+              f'===============================\n'
+              f'TODAY ORDER BLOCK LEVELS\n'
+              f'===============================\n'
+              f'NIFTY SUPPLY LOW LEVEL : {nse_ce_risky_levels}\n'
+              f'===============================\n'
+              f'NIFTY SUPPLY HIGH LEVEL : {nse_ce_safe_levels}\n'
+              f'===============================\n'
+              f'NIFTY DEMAND LOW LEVEL : {nse_pe_risky_levels}\n'
+              f'===============================\n'
+              f'NIFTY DEMAND HIGH LEVEL : {nse_pe_safe_levels}\n'
+              f'===============================\n')
+    
+    asyncio.run(send_telegram_notification(message))  
+
+else: 
+    print("Time not between 9:15 AM - 9:30 AM to show the levels...!")
+
+data = yf.download("^NSEI", period="1mo", interval="5m")
+
+def get_live_price(data):
+    """Fetch the current live price of an index from the last row of data."""
+    return float(data['Close'].iloc[-1])
+
+live_price = round(get_live_price(data),2)
+
+print("live nifty price : ", live_price)
+
+def get_nearest_strike_price(live_price, step):
+    """Calculate the nearest strike price for a given index price."""
+    return round(live_price / step) * step
+
+nearest_strike_nf = get_nearest_strike_price(live_price, 50)
+
+print("Nearest Nifty Strike : ", nearest_strike_nf)
+
+#Keep Running below code from 9AM to 3PM
+if intTime >= 9 and intTime < 15:
+#if intTime >= 18 and intTime < 54:
+    while(intTime<15):
+        c = datetime.now(tz=pytz.timezone('Asia/Kolkata'))
+        runTime = c.strftime('%H:%M:%S')
+        current_hour = int(c.strftime('%H'))
+        current_minute = int(c.strftime('%M'))
+        if current_hour>15:
+            print(f"Exiting the script at {runTime}, as it's past 12 PM.")
+            break;
+        niftyLastPrice = int(live_price)
+        print("NIFTY CMP : ",niftyLastPrice)
+        print(f'==========================\n'
+              f'Running task at:", {runTime}\n'
+              f'NIFTY CMP : {niftyLastPrice}\n'
+              f'NIFTY SUPPLY LOW LEVEL : {nse_ce_risky_levels}\n'
+              f'NIFTY SUPPLY HIGH LEVEL : {nse_ce_safe_levels}\n'
+              f'NIFTY DEMAND HIGH LEVEL : {nse_pe_risky_levels}\n'
+              f'NIFTY DEMAND LOW LEVEL : {nse_pe_safe_levels}\n'
+              f'==========================\n')
+        
+        
+        print("Run Time : ", runTime)
+        counter= counter+1
+        print("Counter : ", counter)
+        
+    
+        if nse_ce_risky_levels <= niftyLastPrice <= nse_ce_safe_levels :
+            buy = 'PE'
+            message=(f'==========================\n'
+                     f'Time : {dt[0]+"-"+runTime}\n'
+                     f'==========================\n'
+                     f'AI-BOT FOR ORDER BLOCK LEVELS\n'
+                     f'==========================\n'
+                     f'NIFYT CMP : {niftyLastPrice}\n'
+                     f'==========================\n'
+                     f'NIFTY TRADING NEAR PE BD LEVEL:{nse_ce_risky_levels}\n'
+                     f'==========================\n'
+                     f'CHOOSE STRIKE : {nearest_strike_nf} {buy}\n'
+                     f'==========================\n'
+                     f'NOTE : ONLY FOR EDUCATIONAL PURPOSE.\n'
+                     f'==========================\n'
+                     f'BOT IS NOT SEBI REG..!\n'
+                     f'==========================\n')
+            asyncio.run(send_telegram_notification(message)) 
+
+        if nse_pe_risky_levels <= niftyLastPrice <= nse_pe_safe_levels :
+            buy = "CE"
+            message=(f'==========================\n'
+                    f'Time : {dt[0]+"-"+runTime}\n'
+                    f'==========================\n'
+                    f'AI-BOT FOR ORDER BLOCK LEVELS\n'
+                    f'==========================\n'
+                    f'NIFYT CMP : {niftyLastPrice}\n'
+                    f'==========================\n'
+                    f'NIFTY TRADING NEAR RISKY CE BO LEVEL:{nse_pe_risky_levels}\n'
+                    f'==========================\n'
+                    f'CHOOSE STRIKE : {nearest_strike_nf} {buy}\n'
+                    f'==========================\n'
+                    f'NOTE : ONLY FOR EDUCATIONAL PURPOSE.\n'
+                    f'==========================\n'
+                    f'BOT IS NOT SEBI REG..!\n'
+                    f'==========================\n')
+            asyncio.run(send_telegram_notification(message))         
+   
+
+        time.sleep(180)
